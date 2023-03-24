2a70bb7a031fcbb715c3c6b8867ee36ddbf4aa6a
jmrrgncpz <paz.jesr@gmail.com>
2023-03-24 17:18:03 +0800

    fix responsiveness of details page

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -12,17 +12,19 @@ const Details = ({ data }: { data: Car }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="max-w-lg mx-auto flex drop-shadow">
-          <div className="flex flex-col md:flex-row gap-5">
-            <figure className="flex-1">
-              <img src={data.img} />
-            </figure>
-            <div className="flex flex-1 flex-col justify-center md:ml-4 mx-5">
-              <span className="text-sm">{vin}</span>
-              <h1 className="text-2xl">
-                {year} {model} | {make}
-              </h1>
-              <span className="text-lg font-bold">{formatPrice(price)}</span>
+        <div className="max-w-lg mx-auto m-10 flex drop-shadow">
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-5 md:flex-row">
+              <figure className="flex-1">
+                <img src={data.img} />
+              </figure>
+              <div className="flex flex-1 flex-col justify-center md:ml-4 mx-5">
+                <span className="text-sm">{vin}</span>
+                <h1 className="text-2xl">
+                  {year} {model} | {make}
+                </h1>
+                <span className="text-lg font-bold">{formatPrice(price)}</span>
+              </div>
             </div>
             <div className="mx-5">
               <h2 className="font-semibold text-lg">Ownership</h2>
