6e81be980040ee6b7531ae650d5edf49d2f158c0
jmrrgncpz <paz.jesr@gmail.com>
2023-03-24 16:55:41 +0800

    add pagination component

---
+++ src/components/PaginationBar/PaginationBar.tsx
@@ -0,0 +1,38 @@
+const ITEM_PER_PAGE = 5;
+
+type Props = {
+  page: number;
+  totalItemCount: number;
+  onChange: (page: number) => void;
+};
+
+const PaginationBar = ({ page, totalItemCount, onChange }: Props) => {
+  const numberOfPages = Math.ceil(totalItemCount / ITEM_PER_PAGE);
+
+  const handlePageClick = (page: number) => () => {
+    onChange(page);
+  };
+
+  return (
+    <div className="flex gap-2 p-2">
+      {Array.from(Array(numberOfPages)).map((_, i) => {
+        const displayPage = i + 1;
+        const isCurrentPage = page === displayPage;
+
+        return (
+          <span
+            onClick={handlePageClick(displayPage)}
+            className={[
+              "px-3 py-1 rounded-full",
+              isCurrentPage ? "border border-gray-700" : "",
+            ].join(" ")}
+          >
+            {displayPage}
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default PaginationBar;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Card } from "./Card";
 export { default as Input } from "./Primitives/Input";
 export { default as Select } from "./Primitives/Select";
 export { default as CarListItem } from "./CarListItem";
+export { default as PaginationBar } from "./PaginationBar";
 
 export { default as Search } from "./Search";
 export type { SearchValue } from "./Search";
