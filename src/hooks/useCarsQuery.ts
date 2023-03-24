be224c7b86a248485e4aee0f04e55c9a42420dd0
jmrrgncpz <paz.jesr@gmail.com>
2023-03-24 16:56:33 +0800

    implement pagination

--- src/hooks/useCarsQuery.ts
+++ src/hooks/useCarsQuery.ts
@@ -1,25 +1,36 @@
 import { useEffect, useState } from "react";
 import { Car } from "../../server/types";
 
-const useCarsQuery = () => {
+const useCarsQuery = (page: number) => {
   const [cars, setCars] = useState<Car[]>();
+  const [totalItemCount, setTotalItemCount] = useState<number>();
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
 
-    fetch("http://localhost:3001/cars")
-      .then((res) => res.json())
-      .then((data) => {
+    fetch(`http://localhost:3001/cars?_page=${page}&_limit=5`)
+      .then((res) => {
+        const xTotalCount = res.headers.get("X-Total-Count") ?? undefined;
+        if (xTotalCount) {
+          setTotalItemCount(parseInt(xTotalCount));
+        }
+
+        return res.json();
+      })
+      .then((data: Car[]) => {
         setCars(data);
       })
       .finally(() => {
         setIsLoading(false);
       });
-  }, []);
+  }, [page]);
 
   return {
-    data: cars,
+    data: {
+      items: cars,
+      totalItemCount,
+    },
     isLoading,
   };
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,42 @@
-import { Search, SearchValue, CarListItem } from "@/components";
+import { Search, SearchValue, CarListItem, PaginationBar } from "@/components";
 import { useCarsQuery } from "@/hooks";
 import Head from "next/head";
+import Link from "next/link";
 import { useState } from "react";
 import { Car } from "../../server/types";
 
 export default function Home() {
   const [searchResult, setSearchResult] = useState<Car[]>();
-
-  const { data: cars, isLoading } = useCarsQuery();
+  const [page, setPage] = useState(1);
+  const { data, isLoading } = useCarsQuery(page);
+  const { items, totalItemCount } = data;
 
   const handleSearchValueChange = (value: SearchValue) => {
-    if (!cars) {
+    if (!items) {
       return [];
     }
 
-    const searchResult = cars.filter(
+    const searchResult = items.filter(
       (c) => c[value.filter as keyof Car] == value.searchTerm
     );
 
     setSearchResult(searchResult);
   };
 
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+
   const renderCarList = () => {
-    const carList = searchResult ?? cars;
+    const carList = searchResult ?? items;
     if (!carList || carList.length === 0) {
       return <span>Nothing to see here</span>;
     }
 
     return carList.map((carData) => (
-      <CarListItem key={carData.vin} {...carData} />
+      <Link href={`/details/${carData.id}`}>
+        <CarListItem key={carData.vin} {...carData} />
+      </Link>
     ));
   };
 
@@ -39,11 +47,18 @@ export default function Home() {
         <meta name="description" content="Hypothetical Car listing" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="w-full flex justify-center items-center">
+      <main className="w-full flex justify-center items-center min-w-sm">
         <div className="max-w-7 flex flex-col">
           <div className="py-2 px-4">
             <Search onChange={handleSearchValueChange} />
           </div>
+
+          <PaginationBar
+            onChange={handlePageChange}
+            totalItemCount={totalItemCount ?? 0}
+            page={page}
+          />
+
           <div className="gap-4 px-4 flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3">
             {isLoading && (
               <span className="text-gray-400 font-semibold">Loading...</span>
