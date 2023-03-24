023d6c5537a3465f014d95348c11056b77a133ae
jmrrgncpz <paz.jesr@gmail.com>
2023-03-24 17:01:57 +0800

    fix test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -4,26 +4,29 @@ import Home from "../pages/index";
 
 jest.mock("../hooks", () => ({
   useCarsQuery: () => ({
-    data: [
-      {
-        id: 3,
-        vin: "AAAAAAAAAA3333333",
-        year: 2023,
-        make: "Mazda",
-        model: "Mazda2",
-        price: 25000,
-        img: "https://upload.wikimedia.org/wikipedia/commons/3/31/Mazda2_%28DJ%29_Homura_1X7A0345.jpg",
-      },
-      {
-        id: 4,
-        vin: "BBBBBBBBBB1111111",
-        year: 2022,
-        make: "Mazda",
-        model: "Mazda3",
-        price: 30000,
-        img: "https://upload.wikimedia.org/wikipedia/commons/8/88/2019_Mazda3_SE-L_2.0_Front.jpg",
-      },
-    ],
+    data: {
+      items: [
+        {
+          id: 1,
+          vin: "AAAAAAAAAA3333333",
+          year: 2023,
+          make: "Mazda",
+          model: "Mazda2",
+          price: 25000,
+          img: "https://upload.wikimedia.org/wikipedia/commons/3/31/Mazda2_%28DJ%29_Homura_1X7A0345.jpg",
+        },
+        {
+          id: 2,
+          vin: "BBBBBBBBBB1111111",
+          year: 2022,
+          make: "Mazda",
+          model: "Mazda3",
+          price: 30000,
+          img: "https://upload.wikimedia.org/wikipedia/commons/8/88/2019_Mazda3_SE-L_2.0_Front.jpg",
+        },
+      ],
+      totalItemCount: 2,
+    },
     isLoading: false,
   }),
 }));
